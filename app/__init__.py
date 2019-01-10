2f02f4c33e69478448622556251bc07071eeced8
Ricardo Delfin <me@rdelfin.com>
2019-01-10 05:37:23 +0000

    Added missing blueprint register for setup path

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,8 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config.from_envvar('SCHEDULR_SETTINGS')
-    from app.modules import test_module, auth
+    from app.modules import test_module, auth, setup
     app.register_blueprint(test_module.bp)
     app.register_blueprint(auth.bp, url_prefix='/auth')
+    app.register_blueprint(setup.bp, url_prefix='/setup')
     return app
