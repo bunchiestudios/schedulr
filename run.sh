2bc6e845d942424581c7ef83b4d7b36c7d40ec0a
Ricardo Delfin <me@rdelfin.com>
2019-01-10 05:24:13 +0000

    Added optional config file parameter to run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
+config_file=$1
+if [ $1 == ""]; then
+    config_file="$(pwd)/config/sample_config.py"
+
 export FLASK_APP=app
 export FLASK_ENV=development
-export SCHEDULR_SETTINGS=$(pwd)/config/sample_config.py
+export SCHEDULR_SETTINGS=$config_file
 flask run
