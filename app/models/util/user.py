c48038f751e8f131393eb79f39ea6a07a722c134
franspaco <franspaco@gmail.com>
2019-01-11 16:15:46 -0600

    get_or_create_user now returns User

--- app/models/util/user.py
+++ app/models/util/user.py
@@ -2,13 +2,12 @@ from app import db
 
 from app.models import User
 
-def get_or_create_user(*, name: str, email: str) -> int:
+def get_or_create_user(*, name: str, email: str) -> User:
     session = db.get_session()
     user = session.query(User).filter(User.email == email).one_or_none()
     if user:
-        return user.id
-
-    session.add(User(name=name, email=email))
+        return user
+    user = User(name=name, email=email)
+    session.add(user)
     session.commit()
-    user = session.query(User).filter(User.email == email).one_or_none()
-    return user.id
+    return user
