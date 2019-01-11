bc98daf36b733502282f6ce6659bc9a1311b2729
franspaco <franspaco@gmail.com>
2019-01-11 17:18:35 -0600

    progress

--- app/modules/home.py
+++ app/modules/home.py
@@ -3,10 +3,11 @@ from app import db
 from app import models
 from flask import Blueprint, render_template
 
-from app.models.util import user as user_util
+from app.helpers import session_helper
 
 bp = Blueprint('home', __name__)
 
 @bp.route('/', strict_slashes=False)
+@session_helper.load_user_if_logged_in
 def home():
     return render_template('base.html', title='Home')
