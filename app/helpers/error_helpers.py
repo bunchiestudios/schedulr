c9a90baadf7a943297b0f29062ae74a5475d0b1a
Ricardo Delfin <me@rdelfin.com>
2019-01-11 20:05:45 -0600

    Added team API's

--- app/helpers/error_helpers.py
+++ app/helpers/error_helpers.py
@@ -9,10 +9,23 @@ def item_not_found(
                 "error": {
                     "code": 1,
                     "msg": f'Could not find {item_name} with '
-                    f'{field_name}="{field_value}"'
+                    f'{field_name}="{field_value}"',
                 }
             }
         ),
         status=404,
         mimetype="application/json",
+    )
+
+def could_not_update(
+    item_name: str, field_name: str, field_value: str
+) -> Response:
+    return Response(
+        {
+            "error": {
+                "code": 2,
+                "msg": f'Could not update {item_name} for '
+                f'"{field_name}={field_value}"',
+            }
+        }
     )
--- app/modules/api.py
+++ app/modules/api.py
@@ -4,7 +4,11 @@ import json
 from app import db
 from app import models
 from app.helpers import error_helpers, session_helper
-from app.models.util import project as project_util, user as user_util
+from app.models.util import (
+    project as project_util,
+    user as user_util,
+    team as team_util,
+)
 
 
 bp = Blueprint('api', __name__)
@@ -30,3 +34,51 @@ def user_get(user_id):
         return jsonify({"id": user.id, "name": user.name, "email": user.email})
 
     return error_helpers.item_not_found("user", "id", str(user_id))
+
+@bp.route('/team/<int:team_id>', methods=['GET'])
+@session_helper.enforce_validate_token_api
+def team_get(team_id):
+    team = team_util.get_from_id(team_id)
+
+    if team:
+        return jsonify(
+            {
+                "id": team.id,
+                "name": team.name,
+                "team_members": [user.id for user in team.users],
+                "projects": [project.id for project in team.projects],
+            }
+        )
+
+    return error_helpers.item_not_found("team", "id", str(team_id))
+
+@bp.route('/team', methods=['POST'])
+@session_helper.enforce_validate_token_api
+def team_create():
+    team = team_util.create(request.args['name'])
+
+    return jsonify(
+        {
+            "id": team.id,
+            "name": team.name,
+            "team_members": [user.id for user in team.users],
+            "projects": [project.id for project in team.projects],
+        }
+    )
+
+@bp.route('/user/team', methods=['POST'])
+@session_helper.enforce_validate_token_api
+def set_team():
+    user = user_util.set_team(request.args['user_id'], request.args['team_id'])
+
+    if user:
+        return jsonify(
+            {
+                "id": user.id,
+                "name": user.name,
+                "email": user.email,
+                "team_id": user.team.id
+            }
+        )
+
+    return error_helpers.could_not_update('user', 'id', request.args['user_id'])
