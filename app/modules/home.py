9b42b27ef591f8a4c3b329cce27104272dd46d97
franspaco <franspaco@gmail.com>
2019-01-11 22:17:15 -0600

    Home functional

--- app/modules/home.py
+++ app/modules/home.py
@@ -11,6 +11,16 @@ bp = Blueprint('home', __name__)
 @session_helper.load_user_if_logged_in
 def home():
     if g.user:
-        if not g.user.get_team(): # TODO: fix this, team should not be used
+        if not g.user.team:
             return redirect(url_for('team.join'))
+        else:
+            return redirect(url_for('team.root'))
     return render_template('base.html', title='Home')
+
+@bp.route('/login')
+def login_redirect():
+    return redirect(url_for('auth.login'))
+
+@bp.route('/logout')
+def logout_redirect():
+    return redirect(url_for('auth.logout'))
