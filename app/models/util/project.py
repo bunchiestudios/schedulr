3bc91198eaa95df478240e4a577f7fbb6f87682e
Ricardo Delfin <me@rdelfin.com>
2019-01-11 20:24:29 -0600

    Added project create

--- app/models/util/project.py
+++ app/models/util/project.py
@@ -1,7 +1,7 @@
 from typing import Optional
 
 from app import db
-from app.models import Project
+from app.models import Project, Team
 
 def get_project_by_id(id: int) -> Optional[Project]:
     """
@@ -12,3 +12,24 @@ def get_project_by_id(id: int) -> Optional[Project]:
     """
     session = db.get_session()
     return session.query(Project).filter(Project.id == id).one_or_none()
+
+
+def add_project(name: str, team_id: int) -> Optional[Project]:
+    """
+    Adds a project into the DB and returns that same project.
+
+    :param name: Human-readable name of the project
+    :param team_id: The ID of the team this project is assigned to.
+    :return: The created project, or none if there was an issue.
+    """
+    session = db.get_session()
+    team = session.query(Team).filter(Team.id == team_id).one_or_none()
+
+    if not team:
+        return None
+
+    project = Project(name=name, team=team)
+    session.add(project)
+    session.commit()
+
+    return project
