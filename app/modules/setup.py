318fab25fec4a300b3f807aa7c1853daf7354c9e
Ricardo Delfin <me@rdelfin.com>
2019-02-05 00:10:16 +0000

    Added endpoints for clear and setup db, limited out as test API's

--- app/modules/setup.py
+++ app/modules/setup.py
@@ -1,10 +1,24 @@
+from flask import Blueprint, jsonify
+
 from app import db
 from app import models
-from flask import Blueprint
+from app.helpers import session_helper
+from app.models import Base
 
-bp = Blueprint('setup', __name__)
+bp = Blueprint("setup", __name__)
 
-@bp.route('/', methods=['POST'], strict_slashes=False)
+
+@bp.route("/", methods=["POST"], strict_slashes=False)
+@session_helper.test_api
 def setup():
     models.Base.metadata.create_all(db.get_engine())
-    return "<h1> Created DB successfully! </h1>"
+    return jsonify({"result": "ok"})
+
+
+@bp.route("/clear_db", methods=["POST"])
+@session_helper.test_api
+def clear_db():
+    Base.meta.drop_all(
+        bind=db.get_engine(), tables=[t.__table__ for t in Base.__subclasses__()]
+    )
+    return jsonify({"result": "ok"})
