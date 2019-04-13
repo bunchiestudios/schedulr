7432042df74aad7be6b951176c9a76dbdba7b037
franspaco <franspaco@gmail.com>
2019-04-12 21:34:41 -0500

    Fix for azure https

--- app/helpers/oauth_helper.py
+++ app/helpers/oauth_helper.py
@@ -2,8 +2,8 @@ from app.libs.oauth import MsftOauth, GoogleOauth
 from flask import current_app, url_for
 
 
-def __get_redirect_uri_msft():
-    return url_for("auth.msft_callback", _external=True)
+def __get_redirect_uri_msft(scheme=None):
+    return url_for("auth.msft_callback", _external=True, _scheme=scheme)
 
 
 def get_oauth_client_msft() -> MsftOauth:
@@ -11,12 +11,12 @@ def get_oauth_client_msft() -> MsftOauth:
         client_id=current_app.config["MSFT_APP_ID"],
         client_secret=current_app.config["MSFT_APP_SECRET"],
         scope="openid User.Read",
-        redirect_uri=__get_redirect_uri_msft(),
+        redirect_uri=__get_redirect_uri_msft(current_app.config["URL_SCHEME"]),
     )
 
 
-def __get_redirect_uri_google():
-    return url_for("auth.google_callback", _external=True)
+def __get_redirect_uri_google(scheme=None):
+    return url_for("auth.google_callback", _external=True, _scheme=scheme)
 
 
 def get_oauth_client_google() -> GoogleOauth:
@@ -24,5 +24,5 @@ def get_oauth_client_google() -> GoogleOauth:
         client_id=current_app.config["GOOGLE_APP_ID"],
         client_secret=current_app.config["GOOGLE_APP_SECRET"],
         scope="openid profile email",
-        redirect_uri=__get_redirect_uri_google(),
+        redirect_uri=__get_redirect_uri_google(current_app.config["URL_SCHEME"]),
     )
